cfc47ce20d2d16d32bae9a32b8cb652aade11772
Сергей Яныгин <yanyginsa@gmail.com>
2021-09-13 22:55:33 +0300

    Вынос 1с библиотек в корень

---
+++ lib/install-libs.bat
@@ -0,0 +1,6 @@
+call mvn install:install-file -Dfile=com._1c.v8.core-1.0.30-SNAPSHOT.jar -DgroupId=com._1c.v8 -DartifactId=com._1c.v8.core -Dversion=1.0.30-SNAPSHOT -Dpackaging=jar
+call mvn install:install-file -Dfile=com._1c.v8.ibis.admin-1.6.7.jar -DgroupId=com._1c.v8 -DartifactId=com._1c.v8.ibis.admin -Dversion=1.6.7 -Dpackaging=jar
+call mvn install:install-file -Dfile=com._1c.v8.ibis.swp-1.1.1-SNAPSHOT.jar -DgroupId=com._1c.v8 -DartifactId=com._1c.v8.ibis.swp -Dversion=1.1.1-SNAPSHOT -Dpackaging=jar
+call mvn install:install-file -Dfile=com._1c.v8.ibis-1.1.1-SNAPSHOT.jar -DgroupId=com._1c.v8 -DartifactId=com._1c.v8.ibis -Dversion=1.1.1-SNAPSHOT -Dpackaging=jar
+call mvn install:install-file -Dfile=com._1c.v8.swp.netty-1.0.3-SNAPSHOT.jar -DgroupId=com._1c.v8 -DartifactId=com._1c.v8.swp.netty -Dversion=1.0.3-SNAPSHOT -Dpackaging=jar
+call mvn install:install-file -Dfile=com._1c.v8.swp-1.0.3-SNAPSHOT.jar -DgroupId=com._1c.v8 -DartifactId=com._1c.v8.swp -Dversion=1.0.3-SNAPSHOT -Dpackaging=jar
